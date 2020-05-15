9b7d3a10431c2ad8dcea5540a85f61ae43aec703
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-05-14 23:50:33 -0400

    update package

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 require('dotenv').config();
 const server = require('./server');
 
+// console.log(process.env);
+
 const port = process.env.PORT || 4000;
+console.log("port", port);
 
 server.listen(port, () => {
     console.log(`Server listening on localhost:${port}`)
