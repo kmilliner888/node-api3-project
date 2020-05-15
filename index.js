62db94a2b929eb1dba0bbea04d1f54be78e5252f
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-05-14 23:37:25 -0400

    updated server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // code away!
+require('dotenv').config();
 const server = require('./server');
 
-server.listen(4000, () => {
-    console.log('listening on localhost:4000')
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`Server listening on localhost:${port}`)
 });
